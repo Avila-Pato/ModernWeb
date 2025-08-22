bd61d305b411d4a010b69c11d3eab5ef9688ccdb
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-08-21 20:15:01 -0400

    Implement Blog and Contact pages with responsive layouts and form functionality

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { blogs } from "../assets/data";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="bg-gradient-to-r from-[#fffbee] to-white py-28">
+      <div className="max-padd-container">
+        {/* Contenedor */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {blogs.map((blog, index) => (
+            <div key={index} className="relative">
+              <div className="bg-secondary/10 p-4 rounded-2xl">
+                <img
+                  src={blog.image}
+                  alt=""
+                  className="shadow-xl shadow-slate-900/2  rounded-xl"
+                />
+              </div>
+              {/* Informacion */}
+              <p className="medium-14 mt-6">{blog.category}</p>
+              <h5 className="h5 pr-4 mb-1 line-clamp-2">{blog.title}</h5>
+              <p>{blog.description}</p>
+              <button className="underlin mt-2 bold-14 line-clamp-2">
+                Continuar Leyendo
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/data";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="bg-gradient-to-r from-[#fffbee] to-white py-28">
+      <form className="flex flex-col items-center text-sm text-slate-800">
+        <p className="text-xs bg-black/80 text-white font-medium px-3 py-1 rounded-full">
+          Contactanos
+        </p>
+        <h1 className="text-4xl font-bold py-4 text-center">
+          Let’s Get In Touch.
+        </h1>
+        <p className="max-md:text-sm text-gray-500 pb-10 text-center">
+          Or just reach out manually to us at{" "}
+          <a href="#" className="text-secondary hover:underline">
+            hello@prebuiltui.com
+          </a>
+        </p>
 
-export default Contact
+        <div className="max-w-96 w-full px-4">
+          <label htmlFor="name" className="font-medium">
+            Full Name
+          </label>
+          <div className="flex items-center mt-2 mb-4 h-10 pl-3 border border-slate-300 bg-secondary/10 rounded-full focus-within:ring-2 focus-within:ring-black/80 transition-all overflow-hidden">
+            <img  src={assets.user} alt="" width={19}/>
+            <input
+              type="text"
+              className="h-full px-2 w-full outline-none bg-transparent"
+              placeholder="Enter your full name"
+              required
+            />
+          </div>
+
+          <label htmlFor="email-address" className="font-medium mt-4">
+            Email Address
+          </label>
+          <div className="flex items-center mt-2 mb-4 h-10 pl-3 border border-slate-300 bg-secondary/10 rounded-full focus-within:ring-2 focus-within:ring-indigo-400 transition-all overflow-hidden">
+            <img  src={assets.mail} alt="" width={18}/>
+            <input
+              type="email"
+              className="h-full px-2 w-full outline-none bg-transparent"
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+
+          <label htmlFor="message" className="font-medium mt-4">
+            Message
+          </label>
+          <textarea
+            rows="4"
+            className="w-full mt-2 p-2 bg-secondary/10 border border-slate-300 rounded-lg resize-none outline-none focus:ring-2 focus-within:ring-indigo-400 transition-all"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+
+          <button
+            type="submit"
+            className="flexCenter gap-1 mt-5 btn-secondary w-full !font-bold"
+          >
+            Submit Form
+            <img  src={assets.right} alt="" width={19}/>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
