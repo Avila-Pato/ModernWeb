69d5bbc4bdde6d8d8332d4bf91933255681123eb
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-08-24 15:14:37 -0400

    Initialize server with Express, CORS, and essential dependencies; set up basic routing and start server on specified port.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(cors());
+
+app,use('/', (req, res) => {
+    res.send('Hello from server');
+});
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
