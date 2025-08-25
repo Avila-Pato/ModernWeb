b3537e4bd42b3a7b4a7616be3aa909ca6d88dd66
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-08-25 17:06:33 -0400

    feat: Integrate Clerk for user management and add webhook handling

---
+++ server/controller/clerkWebhooks.js
@@ -0,0 +1,56 @@
+import { Webhook } from "svix";
+import { User } from "../models/User";
+
+const clerkWebhooks = async (req, res) => {
+    try {
+        const hook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        const headers = {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
+        }
+        await Webhook.verify(JSON.stringify(req, body), headers)
+
+        const {data, type} = req.body;
+
+        switch(type){
+            case "user.created":
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                    role: data.public_metadata.role
+                }
+                await User.create(userData);
+                break;
+        }
+
+         switch(type){
+            case "user.updated":
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                    role: data.public_metadata.role
+                }
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+        }
+
+         switch(type){
+            case "user.deleted":
+                await User.findByIdAndDelete(data.id);
+                break;
+        }
+        res.json({success: true, message: "Webhook received successfully"})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: "Webhook received failed"})
+    }
+}
+
+export default clerkWebhooks;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controller/clerkWebhooks.js';
 
 
 await connectDB()
 
 const app = express();
 app.use(cors());
+
+app.use(express.json());
+app.use(clerkMiddleware())
+
+app.use('/api/clerk', clerkWebhooks)
 
 app.use('/', (req, res) => {
     res.send('Hello from servereeeeeee');
