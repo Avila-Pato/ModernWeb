0f54e1237314bb21ad1e4265fd603d2705f0e1bf
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-08-25 13:50:47 -0400

    feat: Set up MongoDB connection and update server response message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
+import 'dotenv/config'; 
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import connectDB from "./config/mongodb.js";
+
+
+await connectDB()
 
 const app = express();
 app.use(cors());
 
 app.use('/', (req, res) => {
-    res.send('Hello from server');
+    res.send('Hello from servereeeeeee');
 });
 
 const port = process.env.PORT || 4000
